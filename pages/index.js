371af4d714aa951cfe21e6d04c3bdafbe055673a
Leigh Halliday <leighhalliday@gmail.com>
2021-06-13 15:24:21 -0400

    All done

--- pages/index.js
+++ pages/index.js
@@ -10,5 +10,69 @@ const coins = [
 ];
 
 export default function Home() {
-  return <main>PIE</main>;
+  const [active, setActive] = useState(null);
+  const width = 400;
+  const half = width / 2;
+
+  return (
+    <main>
+      <svg width={width} height={width}>
+        <Group top={half} left={half}>
+          <Pie
+            data={coins}
+            pieValue={(data) => data.amount * data.inUSD}
+            outerRadius={half}
+            innerRadius={({ data }) => {
+              const size = active && active.symbol == data.symbol ? 12 : 8;
+              return half - size;
+            }}
+            padAngle={0.01}
+          >
+            {(pie) => {
+              return pie.arcs.map((arc) => {
+                return (
+                  <g
+                    key={arc.data.symbol}
+                    onMouseEnter={() => setActive(arc.data)}
+                    onMouseLeave={() => setActive(null)}
+                  >
+                    <path d={pie.path(arc)} fill={arc.data.color}></path>
+                  </g>
+                );
+              });
+            }}
+          </Pie>
+
+          {active ? (
+            <>
+              <Text textAnchor="middle" fill="#fff" fontSize={40} dy={-20}>
+                {`$${Math.floor(active.amount * active.inUSD)}`}
+              </Text>
+
+              <Text
+                textAnchor="middle"
+                fill={active.color}
+                fontSize={20}
+                dy={20}
+              >
+                {`${active.amount} ${active.symbol}`}
+              </Text>
+            </>
+          ) : (
+            <>
+              <Text textAnchor="middle" fill="#fff" fontSize={40} dy={-20}>
+                {`$${Math.floor(
+                  coins.reduce((acc, coin) => acc + coin.amount * coin.inUSD, 0)
+                )}`}
+              </Text>
+
+              <Text textAnchor="middle" fill="#aaa" fontSize={20} dy={20}>
+                {`${coins.length} Assets`}
+              </Text>
+            </>
+          )}
+        </Group>
+      </svg>
+    </main>
+  );
 }
